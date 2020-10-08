26f420cdf10b6cf94c3ee4c8c8a9cc8d200391f8
Enrique Encarnacion <enrique.encarnacion@cloudcomputing.com.pe>
2020-10-07 22:19:19 -0500

    Sesión 23: 2_poo_interfaces_repositorio[con_manejo_de_excepcion]

--- src/org/aguzman/poointerfaces/repositorio/AbstractaListRepositorio.java
+++ src/org/aguzman/poointerfaces/repositorio/AbstractaListRepositorio.java
@@ -1,6 +1,7 @@
 package org.aguzman.poointerfaces.repositorio;
 
 import org.aguzman.poointerfaces.modelo.BaseEntity;
+import org.aguzman.poointerfaces.repositorio.excepciones.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +20,10 @@ public abstract class AbstractaListRepositorio<T extends BaseEntity> implements 
     }
 
     @Override
-    public T porId(Integer id) {
+    public T porId(Integer id) throws LecturaAccesoDatoException {
+        if(id == null || id <= 0){
+            throw new LecturaAccesoDatoException("Id inválido debe ser > 0");
+        }
         T resultado = null;
         for(T cli: dataSource){
             if(cli.getId() != null && cli.getId().equals(id)){
@@ -27,16 +31,26 @@ public abstract class AbstractaListRepositorio<T extends BaseEntity> implements 
                 break;
             }
         }
+        if(resultado == null){
+            throw new LecturaAccesoDatoException("No existe el registro con id: " + id);
+        }
         return resultado;
     }
 
     @Override
-    public void crear(T t) {
+    public void crear(T t) throws EscrituraAccesoDatoException{
+        if(t == null){
+            throw new EscrituraAccesoDatoException("Error al insertar un objeto null");
+        }
+        if(this.dataSource.contains(t)){
+            throw new RegistroDuplicadoAccesoDatoException("Error el objeto con id "
+                    + t.getId() + " existe en el repositorio");
+        }
         this.dataSource.add(t);
     }
 
     @Override
-    public void eliminar(Integer id) {
+    public void eliminar(Integer id) throws LecturaAccesoDatoException{
         this.dataSource.remove(this.porId(id));
     }
 
